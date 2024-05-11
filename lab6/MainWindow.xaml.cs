ff7cc0a7cf8e9bfd97aaa522c1f18e89f34231d6
sonchpunk <sofa.barbu@bk.ru>
2024-05-11 20:55:50 +0800

    Добавьте файлы проекта.

---
+++ lab6/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        private List<string> playlist = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlaylistBox.SelectedItem != null)
+            {
+                mediaPlayer.Open(new Uri(PlaylistBox.SelectedItem.ToString()));
+                mediaPlayer.Play();
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void LoadFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Audio Files|*.mp3;*.wav;*.wma";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    playlist.Add(filename);
+                    PlaylistBox.Items.Add(filename);
+                }
+            }
+        }
+
+        private void SavePlaylistButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Playlist Files|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllLines(saveFileDialog.FileName, playlist);
+            }
+        }
+
+        private void OpenPlaylistButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Playlist Files|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] files = File.ReadAllLines(openFileDialog.FileName);
+                playlist.Clear();
+                PlaylistBox.Items.Clear();
+                foreach (string file in files)
+                {
+                    playlist.Add(file);
+                    PlaylistBox.Items.Add(file);
+                }
+            }
+        }
+    }
+}
